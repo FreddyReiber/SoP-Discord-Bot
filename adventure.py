df9aba377431d56ae3a23791358296c1c80fcad9
Freddy <ereiber@uci.edu>
2020-10-13 16:09:18 -0700

    First iteration of Schedueling Missing an on remove emoji trigger, but works on add emoji trigger.

--- adventure.py
+++ adventure.py
@@ -1,5 +1,7 @@
-#TODO - Change the simple Dict of Adventurers into a full class!
-class adventure:
+import discord
+from discord.ext import commands
+
+class Adventure:
 
     def __init__(self, nameOfAdv, minPlayers, maxPlayers, ):
         self.name = nameOfAdv
@@ -7,11 +9,26 @@ class adventure:
         self.maxPlayers = maxPlayers
         self.date = "This is a bug"
         self.time = "Also a bug"
+        self.scheduled = False
+        self.lastOutput = ""
 
     def setDateAndTime(self, date, time):
         self.date = date
         self.time = time
+        self.scheduled = True
 
     def getStandardOutput(self):
-        return f"Current list of players for {self.name} on {self.date} at {self.time}"
+        if self.scheduled == True:
+            return f"Current list of players for {self.name} on {self.date} at {self.time}"
+        return f"{self.name} has not been scheduled"
 
+    def setLastOutput(self, lastOutput):
+        self.lastOutput = lastOutput
+
+    def getCurrentOutput(self, users):
+        output = self.getStandardOutput()
+        for user in users:
+            output += '\n' + user.mention
+        output += '\n'
+        self.lastOutput = output
+        return output
--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import discord
 import random
-import adventure
+from adventure import Adventure
 from dotenv import load_dotenv
 
 from discord.ext import commands
@@ -10,16 +10,17 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+EMOJI = os.getenv('DISCORD_CUSTOM_EMOJI')
 
 #creaties the Bot
 bot = commands.Bot(command_prefix='!')
 
 
 #Variables for running of Bot
-dictOfCurrentAdventures = {}
 #Tupels with current output tex
-listOfCurrentAdventures = []
+listOfAdventures = []
 headRole = "Dungeon Master"
+
 
 
 @bot.event
@@ -27,11 +28,11 @@ async def on_ready():
     for guild in bot.guilds:
         if guild.name == GUILD:
             break
-
     print(
         f'{bot.user} has connected to the following guild:\n'
         f'{guild.name} (id: {guild.id})'
     )
+
 #Test commands
 @bot.command(name = 'lol', help = 'This is for testing')
 @commands.has_role(headRole)
@@ -67,48 +68,53 @@ async def on_message(ctx, dice):
 @bot.command(name = "advAdd", help = "Adds a new adventure to the list of available adventures with player count.")
 @commands.has_role(headRole)
 async def on_message(ctx, adventureName, minPlayers: int, maxPlayers: int):
-    dictOfCurrentAdventures[adventureName] = (minPlayers, maxPlayers)
-    await  ctx.send(f'Added {adventureName} with player count {minPlayers} - {maxPlayers}')
+    newAdv = Adventure(adventureName, minPlayers, maxPlayers)
+    listOfAdventures.append(newAdv)
+    await ctx.send(f'Added {adventureName}')
 
 @bot.command(name = "advList", help = "Lists all currently available adventures!")
 async def on_message(ctx):
-    if len(dictOfCurrentAdventures) == 0:
+    if len(listOfAdventures) == 0:
         await ctx.send("It seems there are now playable adventures right now. If you believe this is a bug, please contact Freddy!")
         return
     output = "Current List of all Adventures!\n"
-    for key in dictOfCurrentAdventures:
-        output += f' -{key}\n'
+    for adv in listOfAdventures:
+        output += f' -{adv.name}\n'
     await ctx.send(output)
 
 @bot.command(name = "advPlay", help = "Starts toe ")
 async def on_message(ctx, advName, date, time):
-    if advName not in dictOfCurrentAdventures:
+    listIndex = -1
+    for i in range(0, len(listOfAdventures)):
+        if advName == listOfAdventures[i].name:
+            listIndex = i
+
+    if listIndex == -1:
         await ctx.send("That adventure is not currently in the database!")
         return
-    minPlayers = dictOfCurrentAdventures[advName][0]
-    maxPlayers = dictOfCurrentAdventures[advName][1]
-    output = f"Current list of players for {advName} at {time} on {date}\n{minPlayers} are required to start.\nThe maximum number of players is {maxPlayers}\nCurrent Players:\n"
-    listOfCurrentAdventures.append((output))
+    thisAdv = listOfAdventures[listIndex]
+    thisAdv.setDateAndTime(date, time)
+    output = thisAdv.getStandardOutput()
+    thisAdv.setLastOutput(output)
     await ctx.send(output)
 
 @bot.event
 async def on_reaction_add(ctx, user):
+    if str(ctx.emoji) != EMOJI:
+        return
     inList = -1
-    for i in range(0, len(listOfCurrentAdventures)):
-        if ctx.message.content in listOfCurrentAdventures[i]:
+    for i in range(0, len(listOfAdventures)):
+        if ctx.message.content in listOfAdventures[i].lastOutput:
             inList = i
             break
     if inList == -1:
         return
 
-    if(ctx.message.content in listOfCurrentAdventures[i]):
-        users = await ctx.users().flatten()
-        output = listOfCurrentAdventures[i]
-        for user in users:
-            print(user)
-            output += user.mention + '\n'
-        await ctx.message.edit(content = output, )
-        listOfCurrentAdventures[i] = output
+    thisAdv = listOfAdventures[inList]
+    listOfUsers = await ctx.users().flatten()
+    output = thisAdv.getCurrentOutput(listOfUsers)
+    await ctx.message.edit(content = output )
+
 
 
 #Error handeling
